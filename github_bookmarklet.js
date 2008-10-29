433bae54e0bb13e1d4c2676c8cdefe0567f50586
Douglas F Shearer <dougal.s@gmail.com>
2008-10-29 01:24:47 +0000

    Added compile instructions to source.

--- github_bookmarklet.js
+++ github_bookmarklet.js
@@ -1,6 +1,8 @@
 // Copyright (c) Douglas F Shearer 2008
 // Available under the MIT Licence.
 // Install at: http://douglasfshearer.com/blog/announcing-github-bookmarklet
+//
+// Compile command: perl make_bookmarklet.pl github_bookmarklet.js
 
 var query = prompt("Search GitHub.com");
 
