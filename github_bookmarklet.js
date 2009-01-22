26d909b826e68ac0e51bda840c5967f308763207
Douglas F Shearer <dougal.s@gmail.com>
2009-01-22 23:30:43 +0000

    Added support for repository search. Now making use of the new, better, GitHub search types.

--- github_bookmarklet.js
+++ github_bookmarklet.js
@@ -8,12 +8,16 @@ var query = prompt("Search GitHub.com");
 
 var user_search_ary = /^u:(.*)/g.exec(query);
 var code_search_ary = /^c:(.*)/g.exec(query);
+var repo_search_ary = /^r:(.*)/g.exec(query);
 
 if (user_search_ary) {
-  window.location = "http://github.com/" + user_search_ary[1];
+  window.location = "http://github.com/search?type=Users&q=" + user_search_ary[1];
 }
 else if (code_search_ary) {
-  window.location = "http://github.com/codesearch?q=" + code_search_ary[1] + "&repo=&langOverride=&language=&x=0&y=0";
+  window.location = "http://github.com/search?type=Code&q=" + code_search_ary[1];
+}
+else if (repo_search_ary) {
+  window.location = "http://github.com/search?type=Repositories&q=" + repo_search_ary[1];
 }
 else if (query) {
   window.location = "http://github.com/search?q=" + query;
