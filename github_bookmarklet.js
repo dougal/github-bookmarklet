b55c32be277131ffe70ac404ca648dc294814da5
Douglas F Shearer <dougal.s@gmail.com>
2009-11-04 11:48:57 +0000

    Removed search types due to awesome support for search types on GitHub directly http://github.com/search.

--- github_bookmarklet.js
+++ github_bookmarklet.js
@@ -5,20 +5,6 @@
 // Compile command: perl make_bookmarklet.pl github_bookmarklet.js
 
 var query = prompt("Search GitHub.com");
-
-var user_search_ary = /^u:(.*)/g.exec(query);
-var code_search_ary = /^c:(.*)/g.exec(query);
-var repo_search_ary = /^r:(.*)/g.exec(query);
-
-if (user_search_ary) {
-  window.location = "http://github.com/search?type=Users&q=" + user_search_ary[1];
-}
-else if (code_search_ary) {
-  window.location = "http://github.com/search?type=Code&q=" + code_search_ary[1];
-}
-else if (repo_search_ary) {
-  window.location = "http://github.com/search?type=Repositories&q=" + repo_search_ary[1];
-}
-else if (query) {
+if (query) {
   window.location = "http://github.com/search?q=" + query;
 }
