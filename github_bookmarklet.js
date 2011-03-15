df63b1f98f2b1c4710414ddc716d34e291740ca5
Douglas F Shearer <dougal.s@gmail.com>
2011-03-15 23:20:58 +0000

    Can now go directly to a repository page when a user/repository path is entered.

--- github_bookmarklet.js
+++ github_bookmarklet.js
@@ -1,10 +1,17 @@
-// Copyright (c) Douglas F Shearer 2008
+// Copyright (c) Douglas F Shearer 2008 - 2011
 // Available under the MIT Licence.
 // Install at: http://douglasfshearer.com/blog/announcing-github-bookmarklet
-// 
 // Compile command: perl make_bookmarklet.pl github_bookmarklet.js
 
 var query = prompt("Search GitHub.com");
+var path = "";
 if (query) {
-  window.location = "http://github.com/search?q=" + query;
+  if (query.match(/^[\w\-]+\/[\w\-]+$/)) {
+    path = query;
+  }
+  else {
+    path = "search?q=" + query;
+  }
+
+  window.location = "http://github.com/" + path;
 }
